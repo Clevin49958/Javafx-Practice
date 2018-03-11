250685c59d11a5fbbadc4130bcfc483d00446760
Clevin49958 <hyxkv925@gmail.com>
2018-03-12 08:45:22 +1300

    rename of project

--- Messenger4School/src/main/java/com/m4s/connection/Test.java
+++ Messenger4School/src/main/java/com/m4s/connection/Test.java
@@ -1,9 +1,12 @@
 package com.m4s.connection;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(new IPAddress("192.168.1.86").toString());
+		Scanner input = new Scanner(System.in);
+        
 	}
 
 }
